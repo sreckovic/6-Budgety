0d114cab32c25d4fb2670cd046abb3ba1415a97c
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-31 15:54:50 +0100

    Modules
    Modules setup

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Budget Controller
+var  budgetController = (function() {
+
+
+})();
+
+// UI Controller
+var UIController = (function() {
+
+
+})();
+
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+  
+})(budgetController, UIController);
